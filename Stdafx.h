4fd00758feb4b67d536ac5b9beb0d5dfad373744
capitanminero <adria_comos@hotmail.com>
2015-02-15 12:19:00 +0100

    Modificado stdafx.h para optimizar compilación

--- Stdafx.h
+++ Stdafx.h
@@ -5,3 +5,15 @@
 #pragma once
 
 
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/gpu/gpu.hpp"
+
+#include "ComputerVisionFrameworkNative.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <map>
+#include <iostream>
+#include <sstream>
